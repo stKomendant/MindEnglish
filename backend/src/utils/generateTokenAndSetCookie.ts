b97d15da8361a7ea4a91068e4d52627fee5505d0
stKomendant <penifanta@gmail.com>
2026-08-17 21:16:01 +0100

    fix cookie sameSite for cross-site auth

--- backend/src/utils/generateTokenAndSetCookie.ts
+++ backend/src/utils/generateTokenAndSetCookie.ts
@@ -12,7 +12,7 @@ export const generateTokenAndSetCookie = (
   res.cookie("authToken", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
